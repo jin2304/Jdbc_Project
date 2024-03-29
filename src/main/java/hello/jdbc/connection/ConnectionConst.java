298f7a82c7355cf53697c8876fb6d7ab618fc619
jin2304 <k1017k2@naver.com>
2024-03-29 17:17:32 +0900

    v1.1 connection test

---
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,12 @@
+package hello.jdbc.connection;
+
+
+/**
+ * DB연결정보 (커넥션) 상수 정의
+ */
+public abstract class ConnectionConst {
+
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+}
