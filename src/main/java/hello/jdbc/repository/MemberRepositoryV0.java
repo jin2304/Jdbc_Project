ad5442e87c01e866049b75722e2d1ea74a7b2beb
jin2304 <k1017k2@naver.com>
2024-03-30 13:13:30 +0900

    v1.2 add select query

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -6,6 +6,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -13,6 +14,7 @@ import java.sql.*;
 @Slf4j
 public class MemberRepositoryV0 {
 
+    //회원 저장
     public Member save(Member member) throws SQLException {
         String sql = "insert into member(member_id, money) values(?, ?)";
 
@@ -36,6 +38,41 @@ public class MemberRepositoryV0 {
 
     }
 
+
+    //회원 조회
+    public Member findById(String memberId) throws SQLException{
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();
+
+            if(rs.next()){
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            }else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+
+
+        }catch (SQLException e){
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
+        }
+
+
+    }
     
 
     private void close(Connection con, Statement stmt, ResultSet rs){
