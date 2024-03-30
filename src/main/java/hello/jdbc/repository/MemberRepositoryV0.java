ef49e71e850630f7cc725bf8cdf47fd815e596c6
jin2304 <k1017k2@naver.com>
2024-03-30 21:13:09 +0900

    add update, delete query

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -73,7 +73,55 @@ public class MemberRepositoryV0 {
 
 
     }
-    
+
+
+    //회원 수정
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money=? where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2,  memberId);
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize={}", resultSize);
+        } catch(SQLException e){
+            log.error("db error", e);
+            throw e;
+        } finally {
+            //리소스 정리, 자원을 다 쓰고 직접 닫아줘야 함
+            close(con, pstmt, null);
+        }
+
+    }
+
+
+
+    //회원 삭제
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            pstmt.executeUpdate();
+        } catch(SQLException e){
+            log.error("db error", e);
+            throw e;
+        } finally {
+            //리소스 정리, 자원을 다 쓰고 직접 닫아줘야 함
+            close(con, pstmt, null);
+        }
+
+    }
 
     private void close(Connection con, Statement stmt, ResultSet rs){
         if(rs != null){
--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -5,7 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -18,13 +20,26 @@ class MemberRepositoryV0Test {
     void curd() throws SQLException {
 
         //save
-        Member member = new Member("member4", 10000);
+        Member member = new Member("member6", 10000);
         repository.save(member);
+
 
         //findById
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember={}", findMember);
-        Assertions.assertThat(findMember).isEqualTo(member);
+        assertThat(findMember).isEqualTo(member);
+
+
+        //update :money:10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 
 }
